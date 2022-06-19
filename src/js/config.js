640d6c887241cd0ac7047d5e7304d9c4e40a8e70
abdelrhman ahmed kamal <abdelrhman.ahmed.nassar@gmail.com>
2022-06-19 07:39:21 +0200

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,9 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+
+export const TIMEOUT_SEC = 10;
+
+export const RES_PER_PAGE = 10;
+
+export const KEY = 'ec82c56b-69da-49cd-9f57-69eda8b21118';
+
+export const MODAL_CLOSE_SEC = 2.5;
---
+++ src/js/controller.js
@@ -0,0 +1,162 @@
+import * as model from './model';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
+import bookmarksView from './views/bookmarksView.js';
+import paginationView from './views/paginationView.js';
+import paginationView from './views/paginationView.js';
+import addRecipeView from './views/addRecipeView.js';
+import { MODAL_CLOSE_SEC } from './config';
+///////////////////////////////////////
+
+// API:  https://forkify-api.herokuapp.com/v2
+
+// Path: https://forkify-api.herokuapp.com/api/v2/recipes
+
+// EX:   https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcfb2
+
+// key: 0b8ca7e1-b921-4b75-96a4-16e01809f065
+
+// post:
+
+/*
+IDs: 
+  5ed6604591c37cdc054bc886
+  5ed6604591c37cdc054bc96e
+  5ed6604591c37cdc054bc8fd
+  5ed6604591c37cdc054bcfb2
+  5ed6604591c37cdc054bce0d
+  5ed6604691c37cdc054bd015
+  5ed6604591c37cdc054bcc5b
+*/
+
+/*
+HOT MODULE
+
+if(module.hot) {
+  module.hot.accept();
+}
+*/
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+    if (!id) return;
+    recipeView.renderSpinner();
+
+    // 0) update results view to mark selected results
+    resultsView.update(model.getSearchResultsPage());
+    bookmarksView.update(model.state.bookmarks);
+
+    // 1) loading recipe
+    await model.loadRecipe(id);
+
+    // 2) Rendering recipe bookmarks view
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    recipeView.renderError();
+    console.log(err);
+  }
+};
+
+const controlSearchResults = async function () {
+  try {
+    resultsView.renderSpinner();
+    // 1) get search query from the view
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    // 2) update the search object in the model
+    await model.loadSearchResults(query);
+
+    // 3) render results
+    resultsView.render(model.getSearchResultsPage());
+
+    // 4) Render Initial pagination buttons
+    paginationView.render(model.state.search);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const controlPagination = function (goToPage) {
+  // Render NEW results
+  resultsView.render(model.getSearchResultsPage(goToPage));
+
+  // Render New pagination buttons
+  paginationView.render(model.state.search);
+};
+
+const controlServings = function (newServings) {
+  // update the recipe servings (in state)
+  model.updateServings(newServings);
+
+  // update the recipe view
+  // recipeView.render(model.state.recipe);
+  recipeView.update(model.state.recipe);
+};
+
+const controlAddBookmark = function () {
+  // 1) Add OR remove the bookmark
+  if (!model.state.recipe.bookmarked) {
+    model.addBookmark(model.state.recipe);
+  } else {
+    model.deleteBookmark(model.state.recipe.id);
+  }
+
+  // 2) update Recipe view
+  recipeView.update(model.state.recipe);
+
+  // 3) render bookmarks
+  bookmarksView.render(model.state.bookmarks);
+};
+
+const controlBookmarks = function () {
+  bookmarksView.render(model.state.bookmarks);
+};
+
+const controlAddRecipe = async function (newRecipe) {
+  try {
+    // Show loading spinner
+    addRecipeView.renderSpinner();
+
+    // Upload the new recipe data
+    await model.uploadRecipe(newRecipe);
+    console.log(model.state.recipe);
+
+    // Render recipe
+    recipeView.render(model.state.recipe);
+
+    // Success message
+    addRecipeView.renderMessage();
+
+    // Render bookmark view
+    bookmarksView.render(model.state.bookmarks);
+
+    // Change ID in URL
+    window.history.pushState(null, '', `#${model.state.recipe.id}`);
+
+    // Close form window
+    setTimeout(function () {
+      addRecipeView.toggleWindow();
+    }, MODAL_CLOSE_SEC * 1000);
+  } catch (err) {
+    console.error('💥', err);
+    addRecipeView.renderError(err.message);
+  }
+};
+
+const init = function () {
+  bookmarksView.addHandlerRender(controlBookmarks);
+  recipeView.addHandlerRender(controlRecipes);
+  recipeView.addHandlerUpdateServings(controlServings);
+  recipeView.addHandlerAddBookmark(controlAddBookmark);
+  searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
+  addRecipeView.addHandlerUpload(controlAddRecipe);
+};
+init();
+
+///////
+document.querySelector('.current-year').textContent = new Date().getFullYear();
